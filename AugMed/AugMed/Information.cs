8ed469fdb4a799533c797c23e90818f093aa9cc6
nmrabinovich <nmrabinovich@gmail.com>
2010-12-09 05:02:18 -0500

    Got text rendered on marker and control flow done

---
+++ AugMed/AugMed/Information.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace AugMed
+{
+    public class Information
+    {
+        public String Text;
+        public Vector3 Location;
+        public String Setting;
+        public String Controller;
+    }
+
+    public class Text3D
+    {
+        public String Text;
+        public Vector3 Transform;
+    }
+}
